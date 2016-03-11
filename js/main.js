ce8b4cfba92b091cfe6751b571fe8e38a682e8bf
Hong Shick Pak <hong@hspak.com>
2016-03-10 23:17:33 -0800

    added react-router

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, Link, browserHistory} from 'react-router';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the app container.</h1>
+        {this.props.children}
+      </div>);
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is the homepage.</p>
+        <Link to="/about">Go to About</Link>
+      </div>
+    );
+  }
+}
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>About Page</p>
+        <Link to="/">Go Back Home</Link>
+      </div>);
+  }
+}
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Index} />
+      <Route path="about" component={About} />
+    </Route>
+  </Router>
+);
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  routes,
   document.getElementById("body")
 );
